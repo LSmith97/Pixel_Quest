ecef1e97f6a1212a54ec4e58de4ff5152655c9da
lsmith <therealshaunsred@gmail.com>
2023-07-23 17:25:56 -0400

    created hero class

---
+++ game_files/js/main.js
@@ -0,0 +1,38 @@
+  /*----- constants -----*/
+  class Hero {
+    constructor() {
+        this.level = 0;
+        this.xp = 0;
+        this.atk = 5;
+        this.def = 5
+        this.maxHP = 20;
+        this.maxMana = 10;
+        this.gold = 0;
+
+        this.abilities = [
+            {name: 'Slash',
+            damage: 5,
+            cost: 5},
+
+            {name: 'Fireball',
+            damage: 10,
+            cost: -5},
+
+            {name: 'Spirit Bomb',
+             damage: 15,
+            cost: -10},
+        ]
+    }
+  }
+
+  /*----- state variables -----*/
+
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
+
